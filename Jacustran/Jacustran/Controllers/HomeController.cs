a5ea5cb35eeadce76dc8e0f3c6292c4879361339
Jan Michael Leufke <jan.leufke@outlook.de>
2024-03-12 13:42:42 +0100

    Creating Solution - Adding Clean Architecture Folder Structure - Scaffolding Blazor Template

---
+++ Jacustran/Jacustran/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jacustran.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok("Hello, World from home controller!");
+    }
+}
